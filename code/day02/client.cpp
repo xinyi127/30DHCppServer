f6ddf2746d327726e3626f7de2eb21aaac4f8332
xinyi127 <1224179212@qq.com>
2023-04-26 00:44:08 +0800

    完善了 server.cpp 和 client.cpp，创建并完成了 Makefile

--- code/day02/client.cpp
+++ code/day02/client.cpp
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -19,5 +20,39 @@ int main() {
     int recnt = connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
     errif(recnt == -1, "socket connect error");
 
+    while(true) {
+        // 定义缓冲区，存放从键盘输入到服务器的数据。
+        char buf[1024];
+        // 清空缓冲区。
+        bzero(&buf, sizeof(buf));
+        printf("请输入数据：\n");
+        scanf("%s", buf);
+        // 发送缓冲区中的数据到服务器socket，返回已发送数据大小。
+        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+        // write() 函数会将缓冲区 buf 中的 sizeof(buf) 个字节写入文件 sockfd，成功则返回写入的字节数，失败则返回 -1。
+        if(write_bytes == -1){
+            printf("socket already disconncted, can't write any more!\n");
+            break;
+        }
+        // 清空缓冲区。
+        bzero(&buf, sizeof(buf));
+        // 从服务器 socket 读到缓冲区，返回已读数据大小。
+        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+        // 在 read() 中，<0 代表出错（实际上只用到了 -1），==0 代表 EOF，>0 才是读取成功，此时代表字节数。关于 read() 返回 0：https://www.zhihu.com/question/355020251
+        if(read_bytes > 0) {
+            printf("message from server: %s\n", buf);
+        } else if(read_bytes == 0) { // read 返回 0，表示 EOF，通常是服务器断开连接，等会儿可以通过人为终止服务器连接进行测试
+            printf("server socket disconnected!\n");
+            break;
+        } else if(read_bytes == -1) { // read 返回 -1，表示发生错误，按照上文方法进行错误处理。
+            // 发生错误，等下调用错误处理函数后会自动退出程序，所以需要提前关闭 sockfd。
+            close(sockfd);
+            errif(true, "socket read error");
+        }
+    }
+    close(sockfd);
     return 0;
 }
+
+// 两台计算机之间的通信相当于两个套接字之间的通信.
+// 在服务器端用 write() 向套接字写入数据，客户端就能收到，然后再使用 read() 从套接字中读取出来，就完成了一次通信。
--- code/day02/server.cpp
+++ code/day02/server.cpp
@@ -3,6 +3,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h> // read 和 write 函数
 
 int main() {
     // fd 是 file descriptor 文件描述符。更多关于 socket fd ：https://zhuanlan.zhihu.com/p/399651675。
@@ -36,5 +37,36 @@ int main() {
 
     printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
 
+    // 建立一个socket连接后，使用 <unistd.h> 头文件中 read 和 write 函数来进行网络接口的数据读写操作。
+    // 这两个函数用于 TCP 连接。如果是 UDP，需要使用 sendto 和 recvfrom。
+    while (true) {
+        // 定义缓冲区，存放从客户端读到的数据。
+        char buf[1024];
+        // 清空缓冲区。
+        bzero(&buf, sizeof(buf));
+        // 使用 write() 可以向套接字中写入数据，使用 read() 可以从套接字中读取数据。
+        // size_t 是通过 typedef 声明的 unsigned int 类型；ssize_t 在 "size_t" 前面加了一个"s"，代表 signed，即 ssize_t 是通过 typedef 声明的 signed int 类型。
+        // read() 的原型：ssize_t read(int fd, void *buf, size_t nbytes);
+        // fd 为要读取的文件的描述符，buf 为要接收数据的缓冲区地址，nbytes 为要读取的数据的字节数。
+        ssize_t read_bytes = read(clnt_sockfd, buf, sizeof(buf));
+        // read() 函数会从 fd 文件中读取 nbytes 个字节并保存到缓冲区 buf，成功则返回读取到的字节数。
+        if(read_bytes > 0){
+            printf("message from client fd %d: %s\n", clnt_sockfd, buf);
+            // 将相同数据写回客户端。
+            // write() 的原型：ssize_t write(int fd, const void *buf, size_t nbytes);
+            // fd：要写入的文件的描述符。buf：要写入的数据的缓冲区地址。nbytes：要写入的数据的字节数。
+            write(clnt_sockfd, buf, sizeof(buf));
+        } else if(read_bytes == 0){ // read() 返回 0 表示 EOF ，规定如此。通常是对端关闭了连接。
+            printf("client fd %d disconnect\n", clnt_sockfd);
+            // 小细节：Linux 系统的文件描述符理论上是有限的，在使用完一个 fd 之后，需要使用头文件 <unistd.h> 中的 close 函数关闭。
+            close(clnt_sockfd);
+            break;
+        } else if(read_bytes == -1){ // read() 返回 -1，表示发生错误。
+            close(clnt_sockfd);
+            errif(true, "socket read error");
+        }
+    }
+    // 使用完一个 fd 后应该及时关闭。
+    close(sockfd);
     return 0;
 }
