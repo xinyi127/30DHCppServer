46bd7f1e3817b9e09371058b5e86ed23074c8afd
xinyi127 <1224179212@qq.com>
2023-05-21 23:49:52 +0800

    添加 Makefile 文件；完善 server.cpp 文件，使服务器支持多个客户端连接

--- code/day03/server.cpp
+++ code/day03/server.cpp
@@ -2,8 +2,10 @@
 #include <sys/socket.h> // socket 及相关函数和数据结构
 #include <arpa/inet.h> // IP 地址转换相关的函数和宏，该头文件引入了 <netinet/in.h>
 #include <sys/epoll.h> // 创建 epoll 实例，向实例中添加、修改或删除监视的文件描述符，等待 I/O 事件的发生等
-#include <fcntl.h> // 与文件描述符相关的常量、类型和函数，例如 fcntl
+#include <fcntl.h> // 与文件描述符相关的常量、类型和函数，例如 fcntl()
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h> // close() 函数
 
 #define MAX_EVENTS 1024
 
@@ -44,6 +46,36 @@ int main() {
     // 关于 epoll_ctl 函数：https://www.cnblogs.com/dream-chaser/p/7401184.html
     epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev); // 添加事件到 epoll
 
+    while(true) {
+        // 返回发生事件的文件描述符的个数,这些文件描述符都是已经注册到 epoll 实例中的文件描述符
+        int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1); // 第四个参数表示等待时间， -1 表示永久等待，0 表示立即返回
+        errif(nfds == -1, "epoll wait error");
+        // 发生的事件信息均被存在 events 中，共 nfds 个，遍历所有事件
+        for (int i = 0; i < nfds; i ++) {
+            if (events[i].data.fd == sockfd) { // 发生该事件的文件描述符是 sockfd，说明有新的客户端连接请求
+                struct sockaddr_in clnt_addr;
+                bzero(&clnt_addr, sizeof(clnt_addr));
+                socklen_t clnt_addr_len = sizeof(clnt_addr);
 
+                // 调用 accept 会阻塞，直到有客户端连接到服务器
+                int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len); // 注意 accept 原型的后两个参数均为指针
+                errif(clnt_sockfd == -1, "socket accept error");
+                // inet_ntoa 不需要指定 sin_addr 具体的成员，而 inet_addr 则需要
+                printf("new client fd %d! IP: %s, Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+
+                // 将 clnt_sockfd 添加到 epoll，并指明监听的事件类型
+                bzero(&ev, sizeof(ev));
+                ev.data.fd = clnt_sockfd;
+                // 对于客户端连接，使用 ET 模式，可以让 epoll 更加高效，支持更多并发，但实际上接受连接最好不要用 ET 模式
+                ev.events = EPOLLIN | EPOLLET;
+                setnonblocking(clnt_sockfd); // ET 模式需搭配非阻塞式 socket 使用，非阻塞式 socket 在进行 I/O 操作时，如果没有数据，则立即返回
+                epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sockfd, &ev);
+            }
+        }
+    }
+    close(sockfd); // 记得关闭 socket，释放系统资源
     return 0;
 }
+
+// 开始调用 epoll_wait 时，只有一个文件描述符需要监听，也就是 sockfd，此后当客户端有连接请求时，将客户端的 socket 添加到 epoll，
+// 此时需要监听多个文件描述符，事实上在本程序里，sockfd 只会发生 EPOLLET 事件，而客户端的 socket 只会发生 EPOLLIN 事件。
