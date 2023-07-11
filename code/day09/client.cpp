3b19fde1d4350e41ea682f03414df43fc948bf30
xinyi127 <1224179212@qq.com>
2023-07-11 12:42:52 +0800

    调用自己实现的网络库改写客户端

--- code/day09/client.cpp
+++ code/day09/client.cpp
@@ -1,47 +1,58 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <iostream>
 #include <string.h>
 #include <unistd.h>
 #include "src/util.h"
-
-#define BUFFER_SIZE 1024
+#include "src/Buffer.h"
+#include "src/InetAddress.h"
+#include "src/Socket.h"
 
 int main(){
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(sockfd == -1, "socket create error");
+    // 创建 socket、网络地址、连接服务器
+    Socket* sock = new Socket();
+    InetAddress* addr = new InetAddress("127.0.0.1", 1234);
+    sock->connect(addr);
 
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    // 实际上 sin_addr 只有 s_addr 一个成员变量，之所以要设计成结构体是因为这样可以保证可移植性和可扩展性
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(1234);
+    int sockfd = sock->getFd();
 
-    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect error");
+    // 发送和接收缓冲区
+    Buffer* sendBuffer = new Buffer();
+    Buffer* readBuffer = new Buffer();
 
     while(true){
-        char buf[BUFFER_SIZE];
-        memset(buf, 0, sizeof(buf));
-        scanf("%s", buf);
-        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+        // 向服务器端发送请求
+        sendBuffer->getline();
+        ssize_t write_bytes = write(sockfd, sendBuffer->c_str(), sendBuffer->size());
         if(write_bytes == -1) { // 写数据时出错
-            printf("socket already disconnected, can't write any more!");
+            printf("socket already disconnected, can't write any more!\n");
             break;
         }
 
-        memset(buf, 0, sizeof(buf));
-        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
-        if(read_bytes > 0){
-            printf("message from server: %s\n", buf);
-        } else if(read_bytes == 0){ // 读取到了文件末尾，或者socket连接被关闭
-            printf("server socket disconnected!\n");
-            break;
-        } else if(read_bytes == -1){ // 出错
-            close(sockfd);
-            errif(-1, "socket read error");
+        int already_read = 0; // 读取完数据的标志
+        char buf[1024]; // 大小无所谓，会全部存到读缓冲区。原本是边写边收，现在是全部收完再写
+        while(true){
+            memset(buf, 0, sizeof(buf));
+            ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+            if(read_bytes > 0){
+                readBuffer->append(buf, read_bytes);
+                already_read += read_bytes;
+            } // 如果已经读取完数据，会在输出后退出循环；所以此处是 socket 连接被关闭 
+              else if(read_bytes == 0){
+                printf("server disconnected!\n");
+                exit(EXIT_SUCCESS); // exit() 函数用于终止程序执行并退出
+            } else if(read_bytes == -1){ // 出错
+                errif(-1, "socket read error");
+                exit(EXIT_FAILURE);
+            }
+
+            // 网络中数据是分片传输的，用 >= 判断可以增强鲁棒性
+            if(already_read >= sendBuffer->size()){
+                printf("message from server: %s\n", readBuffer->c_str());
+                break;
+            }
         }
+        readBuffer->clear();
     }
-    close(sockfd);
+    delete addr;
+    delete sock;
     return 0;
 }
