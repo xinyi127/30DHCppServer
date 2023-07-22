24426263adf23af056bea9a9d1c5f608aa16548f
xinyi127 <1224179212@qq.com>
2023-07-22 16:37:04 +0800

    update setBuf function

--- code/day11/src/Buffer.h
+++ code/day11/src/Buffer.h
@@ -16,9 +16,11 @@ private:
 public:
     Buffer();
     ~Buffer();
+
     void append(const char* _str, int _size);
     ssize_t size();
     const char* c_str();
     void clear();
     void getline();
+    void setBuf(const char*);
 };
